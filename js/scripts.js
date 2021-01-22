4ae3601ee4b800b0a3e82f887bff819f8cb63690
Louie Schoenknecht <luisesch97@gmail.com>
2021-01-22 12:57:15 -0800

    mess up code a bit, can't figure out how to make it work. taking break now

--- js/scripts.js
+++ js/scripts.js
@@ -15,10 +15,22 @@ $(document).ready(function() {
   // business logic
 
 //let beepBoop = function(userInput) {
+
+//function replaceNumbers(beepBoop) {
+function beepBoop (maxNumber)
   let numbersArray = []; 
-  for (let i = 0; i <= userInput; i++) {
-  numbersArray.push(i);
-  console.log(numbersArray)
+  for (let i = 0; i <= maxNumber; i++) {
+    let numbers = i.toString();
+    console.log(numbers);
   };
 
-
+  //for (let i = 0; i < numbersArray.length; i++) {
+    if (numbersArray.includes(3)) {
+      
+      let number = numbersArray[i];
+      let beepBoop = number.replace(number, '')
+      let result = beepBoop + "Won't you be my neighbor?";
+      console.log(result)
+    }
+  
+//}
