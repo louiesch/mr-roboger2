3cb52771eed0cf0d868c8d8fd9c0149577684496
Louie Schoenknecht <luisesch97@gmail.com>
2021-01-22 10:34:59 -0800

    add code to pass first test using a for each loop. I'm not happy with it, but it will have to do

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 // business logic
 
+// let number = '5'
+// let numberArray = [];
+// function toArray(number) {
+//   for (i = 0; i <= number; i++) {
+//     numberArray.push(number[i]);
+//     console.log(numberArray);
+//   };
+// };
+
+const input = [1]
 let numberArray = [];
-function toArray(number) {
-  for (i = 0; i < number; i++) {
-    numberArray.push(i);
-  }
-}
+input.forEach(function(number) {
+  console.log(number);
+});
