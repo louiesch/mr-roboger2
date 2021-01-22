94d287b4a1eb43190326c116ef7b7806eb6f95f3
Louie Schoenknecht <luisesch97@gmail.com>
2021-01-22 11:57:03 -0800

    add second test, program now takes entered number, counts down to 0 and lists output in string

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
-// business logic
-
-const 
-
-
 // UI logic
 
 $(document).ready(function() {
-  $("form#formOne").submit(function(event) {
-    event.preventDefault();
-    let number = parseInt($("input#number").val());
-    let output = beepBoop(number);
+  $('form#formOne').submit(function(event) {
+    event.preventDefault()
+    // let userInput = parseInt($('input#userInput').val());
+    // let result
+//     let output = beepBoop(number);
+//     $('#output').text(output);
+//     $('#output').show();
+    });
   });
-});
+  
+  
+  // business logic
+
+let beepBoop = function(userInput) {
+  let numbersString = []; 
+  for (let i = 0; i <= userInput; i++) {
+  numbersString.push(i.toString());
+  console.log(numbersString)
+  };
+};
+
+
+// const toArray = str.split('').map(function(numberList) {
+//   return parseInt(numberList);
+//   console.log(number)
