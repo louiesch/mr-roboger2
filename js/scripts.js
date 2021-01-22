7854388170e5b8aa5d8e8226c0f886cbd0348825
Louie Schoenknecht <luisesch97@gmail.com>
2021-01-22 14:52:23 -0800

    add code in js for program to replace any number containing the digit '3' with my specified phrase. renamed several variables for the umpteenth time to help wrap my brain around what im doing

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,26 @@
 // business logic
 
-function beepBoop(numMax) { 
-  var numbers = [];
-  for (let i = 0; i <= numMax; i++) {
+function beepBoop(number) { 
+  var roboPhrase = [];
+  for (let i = 0; i <= number; i++) {
+    // I struggled with the line below a lot. Ended up deciding to make a string after all, so that I can use the .includes() method.
     var numberString = i.toString();
-    console.log(numberString);
-
-    if (numberString.includes(3))
-
-  }
-}
+    //console.log(numberString);
+    if (numberString.includes(3)) {
+      roboPhrase.push("Won't you be my neighbor?");
+    // it was at this point that I realized I also needed my else statement for the test to work.
+    } else { roboPhrase.push(numberString)};
+      console.log(roboPhrase);
+  };
 
 
 // front end logic
 
 $(document).ready(function() {
-    $("form#formOne").submit(function(event) {
+  $("form#formOne").submit(function(event) {
     event.preventDefault();
-    let numMax = ($("input#number").val()); 
-    var result = beepBoop(numMax);
-      $("#output").text(result);
-    });
-    
+    let number = ($("input#number").val()); 
+    var result = beepBoop(number);
+    $("#output").text(result);
+  });   
 });
