5881d4b26f66c6880e9d8091121c01f700cc58d5
Louie Schoenknecht <luisesch97@gmail.com>
2021-01-22 15:24:33 -0800

    add code in JS to replace any number containing the digit '1' with the phrase 'beep'

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@
 function beepBoop(number) { 
   let roboPhrase = [];
   for (let i = 0; i <= number; i++) {
-    // I struggled with the line below a lot. Ended up deciding to make a string after all, so that I can use the .includes() method.
+    
     let numberString = i.toString();
 
     if (numberString.includes(3)) {
       roboPhrase.push("Won't you be my neighbor?");
     } else if (numberString.includes(2)) {
       roboPhrase.push("Boop!");
-    } else {
-      roboPhrase.push(numberString)
-    }
+    } else if (numberString.includes(1)) {
+      roboPhrase.push("Beep!");
+    } else roboPhrase.push(numberString)
       console.log(roboPhrase);
   };
 };
